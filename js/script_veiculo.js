07fbe448cdc5010bd0196d6ebd95d8a39a4673bd
Guilherme <guilherme.anthonysantos02@gmail.com>
2026-06-22 21:42:49 -0300

    adicionado novos codigos em script_veiculo.js e novos estilos no style_veiculos.css

---
+++ js/script_veiculo.js
@@ -0,0 +1,8 @@
+import { calcularIPVA } from "./script_calculo.js";
+
+const form = document.getElementById("formVeiculo");
+const lista = document.getElementById("listaVeiculos");
+
+const veiculos = [];
+
+form.addEventListener
